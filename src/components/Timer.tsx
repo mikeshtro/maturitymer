80fddbe572a608e54a1fbb362f94099138cb3119
mikeshtro <strajt@edhouse.cz>
2023-04-28 19:21:31 +0200

    Updated label calculation

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -11,12 +11,17 @@ type TimerProps = {
 };
 
 const Timer: Component<TimerProps> = (props) => {
-  const indexes = Array.from({ length: props.parts.length + 1}, (_, index) => index + 1);
+  const labels = [0];
+  for (let index = 0; index < props.parts.length; index++) {
+    const duration = props.parts[index].subParts.reduce((sum, subpart) => sum + subpart, 0);
+    labels.push((labels[index] + duration));
+  }
+
   return (
     <div class={styles.timer}>
       <div class={styles.labels}>
-        <For each={indexes}>
-          {(index) => <div class={styles.label}>{index}</div>}
+        <For each={labels}>
+          {(label) => <div class={styles.label}>{label / 60}</div>}
         </For>
       </div>
       <div class={styles.parts}>
